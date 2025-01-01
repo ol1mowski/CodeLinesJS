b964c014c49d7347ad374d2a379036b816390813
Oliwier <oliwier.markiewicz.dev@gmail.com>
2025-01-01 19:05:59 +0100

    creating starting project structure

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hi' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
