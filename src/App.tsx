cd558584ca194553ca7b278f1b4340dd92dea28e
Oliwier <oliwier.markiewicz.dev@gmail.com>
2025-01-02 07:36:19 +0100

    Header: create header component with mobile menu logic and full RWD

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Header } from './components/Header/Header.component'
 import { HeroSection } from './components/Hero/HeroSection.component'
 
 function App() {
   return (
     <div className="bg-gray-900">
+      <Header />
       <HeroSection />
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
       },
       animation: {
         blob: "blob 7s infinite",
+        gradient: 'gradient 8s linear infinite',
       },
       keyframes: {
         blob: {
@@ -28,6 +29,13 @@ export default {
             transform: "translate(0px, 0px) scale(1)",
           },
         },
+        gradient: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+      },
+      backgroundSize: {
+        '300%': '300%',
       },
     },
   },
