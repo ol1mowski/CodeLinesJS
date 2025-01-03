ea546b679c929f3785ac985a52550f059c3249a3
Oliwier <oliwier.markiewicz.dev@gmail.com>
2025-01-03 07:18:40 +0100

    Progress: Add Progress section with DNA helix animation and user statistics

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Header } from './components/Header/Header.component'
 import { HeroSection } from './components/Hero/HeroSection.component'
 import { FeaturesSection } from './components/Features/FeaturesSection.component'
 import { ChallengesSection } from './components/Challenges/ChallengesSection.component'
-
+import { ProgressSection } from './components/Progress/ProgressSection.component'
 function App() {
   return (
     <div className="bg-gray-900">
@@ -10,6 +10,7 @@ function App() {
       <HeroSection />
       <FeaturesSection />
       <ChallengesSection />
+      <ProgressSection />
     </div>
   )
 }
