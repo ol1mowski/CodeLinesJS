5fb93cbdf477281445261919aae3967211099ae4
Oliwier <oliwier.markiewicz.dev@gmail.com>
2025-01-06 12:03:31 +0100

    style(dashboard): enhance visual hierarchy with gradients, glows and micro-interactions

--- src/components/Dashboard/Dashboard.component.tsx
+++ src/components/Dashboard/Dashboard.component.tsx
@@ -4,7 +4,8 @@ import { DashboardContent } from "./DashboardContent/DashboardContent.component"
 
 const Dashboard = () => {
   return (
-    <main className="bg-gradient-to-b from-gray-900 via-blue-900 to-indigo-900 min-h-screen">
+    <main className="bg-gradient-to-b from-gray-900 via-blue-900 to-indigo-900 backdrop-blur-lg flex flex-col 
+    h-screen">
       <DashboardNavigation />
       <TopNavigation />
       <div className="ml-[100px] mt-20">
