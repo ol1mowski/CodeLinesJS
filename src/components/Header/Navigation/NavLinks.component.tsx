dc64cffae952e8dc93659d744083c3ca6fbbd0bb
Ol1mowski <olimarki123@wp.pl>
2025-01-10 16:26:47 +0100

    refactor(navigation): memoize nav components and use custom color tokens

--- src/components/Header/Navigation/NavLinks.component.tsx
+++ src/components/Header/Navigation/NavLinks.component.tsx
@@ -1,13 +1,15 @@
+import { memo } from 'react';
 import { navigationLinks } from "./navigationData";
 import { NavLink } from "./NavLink.component";
 
-
-export const NavLinks = () => (
+export const NavLinks = memo(() => (
   <nav className="flex items-center gap-10">
-    {navigationLinks.map((item) => (
-      <NavLink key={item.href} href={item.href}>
-        {item.label}
+    {navigationLinks.map(({ href, label }) => (
+      <NavLink key={href} href={href}>
+        {label}
       </NavLink>
     ))}
   </nav>
-);
+));
+
+NavLinks.displayName = 'NavLinks';
