559fa449f3b69b1e7469094a67f74b0da2a8766c
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2025-03-02 13:33:38 +0100

    cors(config) update cors policy

--- server/config/config.js
+++ server/config/config.js
@@ -25,7 +25,9 @@ const config = {
   },
 
   cors: {
-    origin: '*',
+    origin: process.env.NODE_ENV === 'production' 
+      ? process.env.FRONTEND_URL 
+      : 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With']
