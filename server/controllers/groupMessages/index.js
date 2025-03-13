9c2f557c1bd9d9fb06521313fb591e5f5a627898
Ol1mowski <olimarki123@wp.pl>
2025-03-13 18:12:36 +0100

    refactoring(groupMessage) splite group message controller to smaller parts

---
+++ server/controllers/groupMessages/index.js
@@ -0,0 +1,15 @@
+import { getMessagesController } from './getMessages.js';
+import { sendMessageController } from './sendMessage.js';
+import { editMessageController } from './editMessage.js';
+import { deleteMessageController } from './deleteMessage.js';
+import { addReactionController } from './addReaction.js';
+import { reportMessageController } from './reportMessage.js';
+
+export {
+  getMessagesController,
+  sendMessageController,
+  editMessageController,
+  deleteMessageController,
+  addReactionController,
+  reportMessageController
+}; 
