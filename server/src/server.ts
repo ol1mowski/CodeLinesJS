52f66cfec008b8f3db40067ecd1d2b3ae07a9c89
Ol1mowski <olimarki123@wp.pl>
2025-03-30 10:56:37 +0200

    fix vercel api config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import { configureServer } from "./config/server.config.js";
 import { connectDB } from "./config/db.config.js";
 import { configureRoutes } from "./routes/index.js";
@@ -7,6 +7,21 @@ import { configureStaticFiles } from "./middleware/static.middleware.js";
 import errorHandler from "./middleware/error.middleware.js";
 
 const app: Application = express();
+
+// Obsługa CORS dla zapytań OPTIONS
+app.options('*', (req: Request, res: Response) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.status(200).send();
+});
+
+// Logowanie wszystkich zapytań do API
+app.use('/api', (req: Request, res: Response, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.originalUrl}`);
+  next();
+});
 
 configureServer(app);
 
