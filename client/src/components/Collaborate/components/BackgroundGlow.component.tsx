b1e543a971507920255e89702d43285e7dd78574
Ol1mowski <olimarki123@wp.pl>
2025-07-18 21:05:56 +0200

    refactor: remove useless components

--- client/src/components/Collaborate/components/BackgroundGlow.component.tsx
+++
@@ -1,7 +0,0 @@
-export const BackgroundGlow = () => (
-  <>
-    <div className="absolute top-0 left-0 w-full h-full bg-dark opacity-90" />
-    <div className="absolute -top-40 -right-40 w-96 h-96 bg-js rounded-full blur-[150px] opacity-20" />
-    <div className="absolute -bottom-40 -right-40 w-96 h-96 bg-js rounded-full blur-[150px] opacity-20" />
-  </>
-);
